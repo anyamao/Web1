621791af07884462ea0aae77a532b57e7086a9f1
vika <vikashinder@gmail.com>
2026-01-28 00:02:45 +0700

    2

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-[100px] h-[100px] bg-red-500">12</div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss()],
+});
+//here adding plugins and configurations
+//not really used for amsll projects but is used for large ones
